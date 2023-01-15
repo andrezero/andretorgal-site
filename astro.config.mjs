fea5640783a2fd9e07d3875453ac64a06f71e1e1
andrezero <andre.torgal@preply.com>
2023-01-15 19:49:21 +0100

    feature: emojis

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,20 @@ import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import remarkEmoji from 'remark-emoji';
+
+// const p = () => (_ast, vfile) => {
+//     console.log(_ast, vfile);
+// };
+// mdx({
+//     recmaPlugins: [p],
+// }),
 
 export default defineConfig({
     site: 'https://andretorgal.com',
+    markdown: {
+        remarkPlugins: [remarkEmoji],
+    },
     integrations: [
         preact(),
         mdx(),
