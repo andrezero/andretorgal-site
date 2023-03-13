ee95393ec53dbc64b23e905259f678aeaffb1641
andrezero <andre.torgal@preply.com>
2023-03-13 23:35:00 +0100

    Fix: remote images are not being generated in SSG time

--- src/integration/images/utils/getImageFilename.ts
+++ src/integration/images/utils/getImageFilename.ts
@@ -1,17 +1,15 @@
-import GithubSlugger from 'github-slugger';
+import { slug } from 'github-slugger';
 
 import type { ImageTransform } from '../types';
 
 import { IS_REMOTE_IMAGE } from './constants';
-
-const slugger = new GithubSlugger();
 
 export function getImageFilename(src: string, transform: ImageTransform): string {
     const { width, format } = transform;
     // eslint-disable-next-line security/detect-unsafe-regex
     const extensionLessName = src.replace(/\.[a-z]+$/, '');
     const name = IS_REMOTE_IMAGE.test(src)
-        ? `/images/${slugger.slug(extensionLessName)}`
+        ? `/images/${slug(extensionLessName)}`
         : extensionLessName;
     return `${name}-${width}.${format}`;
 }
