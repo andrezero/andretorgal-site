80c3f9cdc13d4c2c1a26910a9878155079160aec
Andre Torgal <hello@andretorgal.com>
2025-01-07 22:50:13 +0100

    tidy up

--- src/customComponents.ts
+++ src/customComponents.ts
@@ -1,5 +1,7 @@
+import Blockquote from '@components/mdx/Blockquote.astro';
 import Figure from '@components/mdx/Figure.astro';
 
 export const components = {
     img: Figure,
+    blockquote: Blockquote,
 };
